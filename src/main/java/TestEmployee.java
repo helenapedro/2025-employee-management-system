b8bc23555d9bc8a387eca4f42ad43ace9b6c8eb0
Helena Pedro <mbeua94@gmail.com>
2025-09-13 21:13:59 -0500

    testing employee
    Created a static method that takes the array of Employees, and the salary. Return
    an array of Employees whose getPayment() < salary.

---
+++ src/main/java/TestEmployee.java
@@ -0,0 +1,31 @@
+package src.main.java;
+
+import src.main.java.data.EmployeeData;
+import src.main.java.model.Employee;
+
+import java.util.Arrays;
+
+public class TestEmployee {
+    public static void main(String[] args) {
+        Employee[] data = EmployeeData.getEmployeeData();
+
+        System.out.println(Arrays.toString(findSalaryList(data, 8000)));
+    }
+
+    public static Employee[] findSalaryList (Employee[] empArray, double salary) {
+        // Return an array of Employees whose getPayment() < salary.
+        if (empArray == null) {
+            throw new IllegalArgumentException("The array should not be empty");
+        }
+
+        int count = 0;
+        Employee[] temp = new Employee[empArray.length];
+
+        for (Employee e : empArray) {
+            if (e != null && e.getPayment() < salary) {
+                temp[count++] = e;
+            }
+        }
+        return Arrays.copyOf(temp, count);
+    }
+}
